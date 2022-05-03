7bf787d34fe607e1f525e5e0a332999c149c5931
EmotePhilia <szymonkda@gmail.com>
2022-05-03 23:37:03 +0200

    Zrobiona nawigacja po stronie + Video section + Price tagi

---
+++ script.js
@@ -0,0 +1,9 @@
+const goToWordpress = ()=>{
+    window.open('http://localhost/wordpress')
+}
+const goToPrices = ()=>{
+    window.scroll(0,750);
+}
+const goToFeatures = ()=>{
+    window.scroll(0,0);
+}
